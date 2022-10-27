f02fea17e01924cf3faeee41b089ae99d81767cc
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2022-10-27 15:49:44 +0200

    Avoid registering twice TRACE ini entries

--- src/extension/php_objects.h
+++ src/extension/php_objects.h
@@ -40,66 +40,69 @@ typedef struct _dd_ini_setting {
     uint16_t name_prefix_len;
     const char *env_name_prefix;
     uint16_t env_name_prefix_len;
+    int avoid_registering_twice;
 } dd_ini_setting;
 
 #define DD_INI_ENV(name_suff, default_value, modifiable, on_modify,            \
-    name_prefix, name_prefix_len, env_name, env_name_len)                      \
+    name_prefix, name_prefix_len, env_name, env_name_len,                      \
+    avoid_registering_twice)                                                   \
     ((dd_ini_setting){(name_suff), (default_value), sizeof(name_suff "") - 1,  \
         sizeof(default_value "") - 1, (modifiable), (on_modify), NULL, 0,      \
-        name_prefix, name_prefix_len, env_name, env_name_len})
+        name_prefix, name_prefix_len, env_name, env_name_len,                  \
+        avoid_registering_twice})
 
 #define DD_APPSEC_INI_ENV(name_suff, default_value, modifiable, on_modify)     \
     DD_INI_ENV(name_suff, default_value, modifiable, on_modify,                \
         APPSEC_NAME_PREFIX, APPSEC_NAME_PREFIX_LEN, APPSEC_ENV_NAME_PREFIX,    \
-        APPSEC_ENV_NAME_PREFIX_LEN)
+        APPSEC_ENV_NAME_PREFIX_LEN, 0)
 
 #define DD_TRACE_INI_ENV(name_suff, default_value, modifiable, on_modify)      \
     DD_INI_ENV(name_suff, default_value, modifiable, on_modify,                \
         TRACE_NAME_PREFIX, TRACE_NAME_PREFIX_LEN, TRACE_ENV_NAME_PREFIX,       \
-        TRACE_ENV_NAME_PREFIX_LEN)
+        TRACE_ENV_NAME_PREFIX_LEN, 1)
 
 #ifdef ZTS
 #    define DD_INI_ENV_GLOB(name_suff, default_value, modifiable, on_modify,   \
         field_name, glob_type, glob_name, name_prefix, name_prefix_len,        \
-        env_name, env_name_len)                                                \
+        env_name, env_name_len, avoid_registering_twice)                       \
         ((dd_ini_setting){(name_suff), (default_value),                        \
             sizeof(name_suff "") - 1, sizeof(default_value "") - 1,            \
             (modifiable), (on_modify), &(glob_name##_id),                      \
             offsetof(glob_type, field_name), name_prefix, name_prefix_len,     \
-            env_name, env_name_len})
+            env_name, env_name_len, avoid_registering_twice})
 #    define DD_APPSEC_INI_ENV_GLOB(name_suff, default_value, modifiable,       \
         on_modify, field_name, glob_type, glob_name)                           \
         DD_INI_ENV_GLOB(name_suff, default_value, modifiable, on_modify,       \
             field_name, glob_type, glob_name, APPSEC_NAME_PREFIX,              \
             APPSEC_NAME_PREFIX_LEN, APPSEC_ENV_NAME_PREFIX,                    \
-            APPSEC_ENV_NAME_PREFIX_LEN)
+            APPSEC_ENV_NAME_PREFIX_LEN, 0)
 #    define DD_TRACE_INI_ENV_GLOB(name_suff, default_value, modifiable,        \
         on_modify, field_name, glob_type, glob_name)                           \
         DD_INI_ENV_GLOB(name_suff, default_value, modifiable, on_modify,       \
             field_name, glob_type, glob_name, TRACE_NAME_PREFIX,               \
             TRACE_NAME_PREFIX_LEN, TRACE_ENV_NAME_PREFIX,                      \
-            TRACE_ENV_NAME_PREFIX_LEN)
+            TRACE_ENV_NAME_PREFIX_LEN, 1)
 #else
 #    define DD_INI_ENV_GLOB(name_suff, default_value, modifiable, on_modify,   \
         field_name, glob_type, glob_name, name_prefix, name_prefix_len,        \
-        env_name, env_name_len)                                                \
+        env_name, env_name_len, avoid_registering_twice)                       \
         ((dd_ini_setting){(name_suff), (default_value),                        \
             sizeof(name_suff "") - 1, sizeof(default_value "") - 1,            \
             modifiable, (on_modify), &(glob_name),                             \
             offsetof(glob_type, field_name), name_prefix, name_prefix_len,     \
-            env_name, env_name_len})
+            env_name, env_name_len, avoid_registering_twice})
 #    define DD_APPSEC_INI_ENV_GLOB(name_suff, default_value, modifiable,       \
         on_modify, field_name, glob_type, glob_name)                           \
         DD_INI_ENV_GLOB(name_suff, default_value, modifiable, on_modify,       \
             field_name, glob_type, glob_name, APPSEC_NAME_PREFIX,              \
             APPSEC_NAME_PREFIX_LEN, APPSEC_ENV_NAME_PREFIX,                    \
-            APPSEC_ENV_NAME_PREFIX_LEN)
+            APPSEC_ENV_NAME_PREFIX_LEN, 0)
 #    define DD_TRACE_INI_ENV_GLOB(name_suff, default_value, modifiable,        \
         on_modify, field_name, glob_type, glob_name)                           \
         DD_INI_ENV_GLOB(name_suff, default_value, modifiable, on_modify,       \
             field_name, glob_type, glob_name, TRACE_NAME_PREFIX,               \
             TRACE_NAME_PREFIX_LEN, TRACE_ENV_NAME_PREFIX,                      \
-            TRACE_ENV_NAME_PREFIX_LEN)
+            TRACE_ENV_NAME_PREFIX_LEN, 1)
 #endif
 
 void dd_phpobj_startup(int module_number);
