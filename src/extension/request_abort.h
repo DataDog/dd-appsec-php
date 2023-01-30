0ec7e1d3d490792e397bf05fb30e2b95a1af3be7
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2023-01-30 17:51:38 +0000

    * Update HTML and JSON blocking templates * Detect response content-type from Accept header * Provide internal API for setting response code and type * Add function to read full file content * Add a few tests for the different content types supported

--- src/extension/request_abort.h
+++ src/extension/request_abort.h
@@ -5,6 +5,14 @@
 // Copyright 2021 Datadog, Inc.
 #pragma once
 
+typedef enum {
+    response_type_auto = 0,
+    response_type_html = 1,
+    response_type_json = 2,
+} dd_response_type;
+
+void set_response(int code, dd_response_type type);
+
 void dd_request_abort_startup(void);
 // noreturn unless called from rinit on fpm
 void dd_request_abort_static_page(void);
