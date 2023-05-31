594ea2d5518f0b85643b5db4b6bf9889cb9bee3a
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2023-05-31 17:56:04 +0200

    Adapt apache2-mod to local tracer

--- tests/docker/apache2-mod/build_image.sh
+++ tests/docker/apache2-mod/build_image.sh
@@ -4,14 +4,25 @@ set -ex
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 source "$SCRIPT_DIR"/../lib.sh
+TRACE_TARGET="remote"
+#TODO allow to change this
+TRACE_PATH="$SCRIPT_DIR/../../.."
 
 function main {
-  local -r version=$1 variant=$2 tracer_version=$3
+  local -r version=$1 variant=$2
+  tracer_version=$3
   if ! image_exists datadog/dd-appsec-php-ci:php-$version-$variant; then
     "$SCRIPT_DIR"/../php/build_image.sh $version $variant
   fi
   if ! image_exists dd-appsec-php-extension:$version-$variant; then
     "$SCRIPT_DIR"/../extension/build_image.sh $version $variant
+  fi
+  if [ "$tracer_version" = "local" ]; then
+    TRACE_TARGET="local"
+    tracer_version="local"
+    if  ! image_exists dd-appsec-php-trace:$version; then
+        "$SCRIPT_DIR"/../trace/build_image.sh $version $variant
+    fi
   fi
   if ! image_exists dd-appsec-php-helper; then
     "$SCRIPT_DIR"/../helper/build_image.sh
@@ -20,7 +31,8 @@ function main {
   docker build -t dd-appsec-php-apache2-mod:$version-$variant-tracer$tracer_version \
     --build-arg PHP_VERSION=$version --build-arg VARIANT=$variant \
     --build-arg TRACER_VERSION=$tracer_version \
-    -f "$SCRIPT_DIR/Dockerfile" "$SCRIPT_DIR/../../.."
+    --target $TRACE_TARGET \
+    -f "$SCRIPT_DIR/Dockerfile" "$TRACE_PATH"
 }
 
 if [[ $# -ne 3 ]]; then
