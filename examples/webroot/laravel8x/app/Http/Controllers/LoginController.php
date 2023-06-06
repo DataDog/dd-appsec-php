0b1f1b26cae9274fd67c94a09b34f2f3d9906563
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2023-06-06 17:50:13 +0200

    Configure laravel testing login

---
+++ examples/webroot/laravel8x/app/Http/Controllers/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+}
--- examples/webroot/laravel8x/database/seeders/DatabaseSeeder.php
+++ examples/webroot/laravel8x/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         DB::table('users')->insert([
+            'name' => 'ciuser',
+            'email' => 'ciuser@example.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
