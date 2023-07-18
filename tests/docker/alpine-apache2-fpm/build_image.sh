bbba69fb812f6c6e4e85d0090723eb433960e2fd
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2023-07-18 15:54:01 +0200

    Adapt pr

--- tests/docker/alpine-apache2-fpm/build_image.sh
+++ tests/docker/alpine-apache2-fpm/build_image.sh
@@ -4,29 +4,19 @@ set -ex
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 source "$SCRIPT_DIR"/../lib.sh
-TRACE_TARGET="remote"
-#TODO allow to change this
-TRACE_PATH="$SCRIPT_DIR/../../.."
 
 function main {
-  local -r version=$1 variant=$2
-  tracer_version=$3
+  local -r version=$1 variant=$2 tracer_version=$3
   if ! image_exists datadog/dd-appsec-php-ci:php-$version-$variant; then
     "$SCRIPT_DIR"/../php/build_image.sh $version $variant
   fi
   if ! image_exists dd-appsec-php-extension:$version-$variant; then
     "$SCRIPT_DIR"/../extension/build_image.sh $version $variant
   fi
-  if [ "$tracer_version" = "local" ]; then
-    TRACE_TARGET="local"
-    tracer_version="local"
-    if  ! image_exists "dd-appsec-php-trace:$version-alpine"; then
-        "$SCRIPT_DIR"/../trace/build_image.sh $version $variant "alpine"
-    fi
-  fi
   if ! image_exists dd-appsec-php-helper; then
     "$SCRIPT_DIR"/../helper/build_image.sh
   fi
+
   alpine_version=""
   if [[ $version =~ ^8\.[0-9]+ ]]; then
     alpine_version="3.16"
@@ -36,8 +26,7 @@ function main {
     --build-arg PHP_VERSION=$version --build-arg VARIANT=$variant \
     --build-arg TRACER_VERSION=$tracer_version \
     --build-arg ALPINE_VERSION=$alpine_version \
-    --target $TRACE_TARGET \
-    -f "$SCRIPT_DIR/Dockerfile" "$TRACE_PATH"
+    -f "$SCRIPT_DIR/Dockerfile" "$SCRIPT_DIR/../../.."
 }
 
 if [[ $# -ne 3 ]]; then
