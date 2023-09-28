9af4cfff60b0301bf4f8d978f86c86bc0f70d9e4
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2023-09-28 15:33:23 +0100

    Remove commented log statement

--- src/extension/ddappsec.c
+++ src/extension/ddappsec.c
@@ -444,8 +444,6 @@ __thread void *unspecnull TSRMLS_CACHE = NULL;
 
 static void _check_enabled()
 {
-    /*mlog(dd_log_error, "Tracer enabled? %s", get_DD_TRACE_ENABLED() ? "true" :
-     * "false");*/
     if (!get_global_DD_APPSEC_TESTING() &&
         (!get_DD_TRACE_ENABLED() || strcmp(sapi_module.name, "cli") == 0 ||
             (sapi_module.phpinfo_as_text != 0))) {
