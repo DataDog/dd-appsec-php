e0a778bcddb5902c8c308d3d35f26f65efa1df87
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2023-11-23 10:16:55 +0100

    Amend pr suggestions

--- src/helper/compression.cpp
+++ src/helper/compression.cpp
@@ -11,17 +11,18 @@
 
 namespace dds {
 
-static constexpr int64_t encoding = -0xf;
-static constexpr int max_round_decompression = 100;
-
+namespace {
+constexpr int64_t encoding = -0xf;
+constexpr int max_round_decompression = 100;
 // Taken from PHP approach
 // https://heap.space/xref/PHP-7.3/ext/zlib/php_zlib.h?r=8d3f8ca1#36
-constexpr size_t COMPRESSED_SIZE_ESTIMATION(size_t in_len)
+static size_t estimate_compressed_size(size_t in_len)
 {
     // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
     return (((size_t)((double)in_len * (double)1.015)) + 10 + 8 + 4 + 1);
     // NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
 }
+} // namespace
 
 // The implementation of this function is based on how PHP does
 //  https://heap.space/xref/PHP-7.3/ext/zlib/zlib.c?r=9afce019#336
@@ -36,14 +37,15 @@ std::optional<std::string> compress(const std::string &text)
 
     if (Z_OK == deflateInit2(&strm, -1, Z_DEFLATED, encoding, MAX_MEM_LEVEL,
                     Z_DEFAULT_STRATEGY)) {
-        ret_string.resize(COMPRESSED_SIZE_ESTIMATION(text.length()), '\0');
+        auto size = estimate_compressed_size(text.length());
+        ret_string.resize(size);
 
         // NOLINTBEGIN(cppcoreguidelines-pro-type-reinterpret-cast)
         strm.next_in = reinterpret_cast<const Bytef *>(text.data());
         strm.next_out = reinterpret_cast<Bytef *>(ret_string.data());
         // NOLINTEND(cppcoreguidelines-pro-type-reinterpret-cast)
         strm.avail_in = text.length();
-        strm.avail_out = ret_string.capacity();
+        strm.avail_out = size;
 
         if (Z_STREAM_END == deflate(&strm, Z_FINISH)) {
             deflateEnd(&strm);
@@ -65,13 +67,9 @@ std::optional<std::string> uncompress(const std::string &compressed)
     size_t used = 0;
     size_t free;
     size_t capacity;
-
-    if (compressed.length() < 1) {
-        return std::nullopt;
-    }
-
     z_stream strm = {};
-    if (Z_OK != inflateInit2(&strm, encoding)) {
+
+    if (compressed.length() < 1 || Z_OK != inflateInit2(&strm, encoding)) {
         return std::nullopt;
     }
 
@@ -90,7 +88,7 @@ std::optional<std::string> uncompress(const std::string &compressed)
         strm.next_out = reinterpret_cast<uint8_t *>(output.data()) + used;
         status = inflate(&strm, Z_NO_FLUSH);
         used += free - strm.avail_out;
-        capacity += (capacity >> 3) + 1;
+        capacity += (output.size() >> 3) + 1;
         output.resize(capacity);
     }
     if (status == Z_STREAM_END) {
